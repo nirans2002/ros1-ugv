4933fb124b31aff302a97ffb7b7eaff758ca309d
Niran S Narayanan <nirans2002@gmail.com>
2023-04-27 11:51:09 +0530

    git1

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/niran/ugv_ws/src
-# Build directory: /home/niran/ugv_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("ugv")
--- build/ugv/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/niran/ugv_ws/src/ugv
-# Build directory: /home/niran/ugv_ws/build/ugv
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
